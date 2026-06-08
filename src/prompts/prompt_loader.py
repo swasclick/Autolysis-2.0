f545363d497cbd2956ce64740eb0f94b840a2170
SwasGit <swastik2707s@gmail.com>
2026-06-09 00:22:43 +0530

    fixed path issue in prompt loader

--- src/prompts/prompt_loader.py
+++ src/prompts/prompt_loader.py
@@ -5,10 +5,11 @@ class PromptLoader:
     def __init__(self):
         pass
     
-    def load_prompt(prompt_name: str) -> str:
+    def load_prompt(self, prompt_name: str) -> str:
         '''
         Function to load a prompt string by its name
         '''
-        PROMPT_DIR = pathlib.Path("prompts")
+        PROMPT_DIR = pathlib.Path(__file__).parent
+        print(f'PROMPT_DIR : {PROMPT_DIR}')
         prompt = (PROMPT_DIR/f'{prompt_name}.txt').read_text(encoding='utf-8')
         return prompt
