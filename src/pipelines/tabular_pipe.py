b8195d9ab7cb601452e5a91bf46b71a28629e14a
SwasGit <swastik2707s@gmail.com>
2026-08-20 23:12:42 +0530

    template for TabularDataAnalyst class

--- src/pipelines/tabular_pipe.py
+++ src/pipelines/tabular_pipe.py
@@ -1,5 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from llm.prompts.prompt_loader import PromptLoader
+from typing import Optional, Union
 
 class TabularDataAnalyst():
+    
+    def __init__():
+        pass
+    
+    def analyse_numeric():
+        pass
+    
+    def analyse_text():
+        pass
+    
+    def analyse_datetime():
+        pass
+    
+    def analyse_categorical():
+        pass
+    
+    def analyse_email():
+        pass
+    
+    def analyse_phone():
+        pass
+    
+    def analyse_url():
+        pass
+    
+    def analyse_geographic():
+        pass
+    
+    def analyse_financial():
+        pass
+    
+    def analyse(metadata:dict) :
+        
